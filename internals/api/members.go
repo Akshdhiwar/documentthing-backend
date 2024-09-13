f2f44a9abafc35b2936357618d748635e1b57536
Akash Dhiwar <akashdhiwar3001@gmail.com>
2024-09-13 10:26:35 +0530

    Update members.go

--- internals/api/members.go
+++ internals/api/members.go
@@ -2,13 +2,13 @@ package api
 
 import (
 	"github.com/Akshdhiwar/simpledocs-backend/internals/controller"
-	"github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
+	// "github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func MemberRoutes(router *gin.RouterGroup) {
 
-	router.Use(middleware.AuthMiddleware, middleware.RoleMiddleware)
+	// router.Use(middleware.AuthMiddleware, middleware.RoleMiddleware)
 
 	// GET api to list all the github members
 	router.GET("/org/:id", controller.GetOrgMembers)
