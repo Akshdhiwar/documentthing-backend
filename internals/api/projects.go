9deed9aafd57f8edd55ae772c6fd729d9885cefe
Akash Dhiwar <akashdhiwar3001@gmail.com>
2024-09-13 10:56:25 +0530

    Update projects.go

--- internals/api/projects.go
+++ internals/api/projects.go
@@ -2,17 +2,17 @@ package api
 
 import (
 	"github.com/Akshdhiwar/simpledocs-backend/internals/controller"
-	"github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
+	// "github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func ProjectRoutes(router *gin.RouterGroup) {
 
-	 router.Use(middleware.AuthMiddleware)
+	 // router.Use(middleware.AuthMiddleware)
 
 	// POST route to create new project and repo in github
 	router.POST("/create-project", controller.CreateNewProject)
 
 	// GET route to get the user projects
-	router.GET("/get-project", middleware.AuthMiddleware, controller.GetProjects)
+	router.GET("/get-project", controller.GetProjects)
 }
