97e14e7eec55a26fd1a1f47bcf219e9f3ac656af
Akash Dhiwar <akashdhiwar3001@gmail.com>
2024-09-13 10:53:52 +0530

    Update invite.go

--- internals/api/invite.go
+++ internals/api/invite.go
@@ -2,13 +2,13 @@ package api
 
 import (
 	"github.com/Akshdhiwar/simpledocs-backend/internals/controller"
-	"github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
+	// "github.com/Akshdhiwar/simpledocs-backend/internals/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func InviteRoutes(router *gin.RouterGroup) {
 
-	router.Use(middleware.AuthMiddleware)
+	// router.Use(middleware.AuthMiddleware)
 
 	// api to invite a user to project
 	router.POST("/create", controller.CreateInvite)
