b6539e80e803a876e8456f03bd3eda961b33ca9f
Akash Dhiwar <akashdhiwar3001@gmail.com>
2025-01-31 11:37:29 +0530

    Update cors.go

--- internals/utils/cors.go
+++ internals/utils/cors.go
@@ -7,7 +7,7 @@ import (
 
 func Cors() gin.HandlerFunc {
 	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:5173", "https://simpledocs.vercel.app", "https://www.documentthing.com"} // List specific origins
+	config.AllowOrigins = []string{"http://localhost:5173", "https://simpledocs.vercel.app", "https://www.documentthing.com", "https://documentthing.com"} // List specific origins
 	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE"}                                                        // Allow specific methods
 	config.AllowHeaders = []string{"X-User-Id", "X-Project-Id", "Content-Type", "Authorization"}                          // Include custom headers
 	config.AllowCredentials = true                                                                                        // Allow credentials (cookies/authorization headers)
